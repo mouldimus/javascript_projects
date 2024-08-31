8feafbedb596407ec8f9420f39ddf62cf9b7bfa0
mouldimus <thomas@sportstore.co.uk>
2024-08-31 16:08:24 +0100

    Concatenation String Challenge

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -2,4 +2,4 @@ let message = "Hello World";
 window.alert(message);
 let title = document.title;
 let A = "<h1>" + title + ":</h1><p>\"A day without sunshine is like, you know, night.\"<br>- Steve Martin</p>";
-document.write(A);
+document.write(A); // oops I already did that :/
