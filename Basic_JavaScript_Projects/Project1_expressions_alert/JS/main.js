2740803635d779659466b485fa72a7946f004a20
mouldimus <thomas@sportstore.co.uk>
2024-08-31 16:06:06 +0100

    Concatenation Assignment

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,4 +1,5 @@
 let message = "Hello World";
 window.alert(message);
-let A = "<h1>Document Write:</h1><p>\"A day without sunshine is like, you know, night.\"<br>- Steve Martin</p>";
+let title = document.title;
+let A = "<h1>" + title + ":</h1><p>\"A day without sunshine is like, you know, night.\"<br>- Steve Martin</p>";
 document.write(A);
