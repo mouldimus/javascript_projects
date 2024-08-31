00cf9c7023889dd4a9fefea9122d907219b46c37
mouldimus <thomas@sportstore.co.uk>
2024-08-31 16:03:33 +0100

    Escape Character Assignment

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,4 +1,4 @@
 let message = "Hello World";
 window.alert(message);
-let A = "Document Write";
+let A = "<h1>Document Write:</h1><p>\"A day without sunshine is like, you know, night.\"<br>- Steve Martin</p>";
 document.write(A);
