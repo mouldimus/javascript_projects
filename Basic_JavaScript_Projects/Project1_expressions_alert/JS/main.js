812e3d06669fa576b937e3e28b40927df16656f3
mouldimus <thomas@sportstore.co.uk>
2024-09-07 13:25:52 +0100

    Expression Assignment

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,5 +1,8 @@
 let message = "Hello World",
 	title = document.title,
 	A = "<h1>" + title + ":</h1><p>\"A day without sunshine is like, you know, night.\"<br>- Steve Martin</p>";
+
+url = 'https://github.com/mouldimus/javascript_projects'
+
 window.alert(message);
 document.write(A); // oops I already did that :/
