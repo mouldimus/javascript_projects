a53fbea53fa3b38f8df2f8ffdf27969aad68041e
mouldimus <thomas@sportstore.co.uk>
2024-12-01 12:55:54 +0000

    Added comments

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,8 +1,11 @@
 let message = "Hello World",
-	title = document.title,
-	A = "<h1>" + title + ":</h1><p>\"A day without sunshine is like, you know, night.\"<br>- Steve Martin</p>";
+  title = document.title,
+  A =
+    "<h1>" +
+    title +
+    ':</h1><p>"A day without sunshine is like, you know, night."<br>- Steve Martin</p>';
 
-url = 'https://github.com/mouldimus/javascript_projects'
+url = "https://github.com/mouldimus/javascript_projects";
 
 window.alert(message);
-document.write(A); // oops I already did that :/
+document.write(A); // this writes a header element and a paragraph to the file
