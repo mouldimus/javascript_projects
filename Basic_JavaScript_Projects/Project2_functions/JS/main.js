ee882fda913ec1d51910a77199471242fe121c5d
mouldimus <thomas@sportstore.co.uk>
2024-12-02 16:48:36 +0000

    Function Assignment

---
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+function My_First_Function() {
+  let str = "This is the button text!";
+  document.getElementById("Button_Text").innerHTML = str;
+
+  let pStr = "This is the paragraph text!";
+  document.getElementById("appear").innerHTML = pStr;
+}
