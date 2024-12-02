63c715cdf05e7ec7673e90f150efdbbe7f90eb4a
mouldimus <thomas@sportstore.co.uk>
2024-12-02 16:55:25 +0000

    Project2_Functions Assignment

--- Basic_JavaScript_Projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -1,7 +1,10 @@
 function My_First_Function() {
   let str = "This is the button text!";
+  // Change the button text
   document.getElementById("Button_Text").innerHTML = str;
 
-  let pStr = "This is the paragraph text!";
-  document.getElementById("appear").innerHTML = pStr;
+  // concatenation
+  let pStr = "This is the paragraph text! ";
+  let catStr = "This has been concatenated!";
+  document.getElementById("appear").innerHTML = pStr += catStr;
 }
