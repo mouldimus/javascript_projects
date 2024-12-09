9fe8e458c708d1f7375826e5d0cb26e2b8b82693
mouldimus <thomas@sportstore.co.uk>
2024-12-09 16:24:27 +0000

    Infinity Assignment

--- Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -3,3 +3,8 @@ let n = 10;
 document.writeln("s: ", s, typeof s);
 document.writeln("n: ", n, typeof n);
 document.writeln("s + n: ", s + n, typeof (s + n));
+
+let inf = 2e310;
+let negInf = -2e310;
+document.writeln("infinity: ", inf, typeof inf);
+document.writeln("negative infinity: ", negInf, typeof negInf);
