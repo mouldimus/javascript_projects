c326ff7eb7bf56632b5729f624e00cf6a819f272
mouldimus <thomas@sportstore.co.uk>
2024-12-09 16:43:44 +0000

    Triple Equal Signs Assignment

--- Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -11,3 +11,7 @@ document.writeln("negative infinity: ", negInf, typeof negInf);
 document.writeln("5 < 3: ", 5 < 3);
 console.log("5 < 3: ", 5 < 3);
 console.log("s == n: ", s == n);
+console.log(5 === 5);
+console.log(5 === "6");
+console.log(5 === "5");
+console.log(5 === 6);
