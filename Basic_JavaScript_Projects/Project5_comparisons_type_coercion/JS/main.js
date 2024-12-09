7b1803ec963a8e9019f0a70290200bb78d9b920e
mouldimus <thomas@sportstore.co.uk>
2024-12-09 16:01:28 +0000

    Type Coercion Assignment

--- Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,1 +1,5 @@
-document.writeln(typeof "10");
+let s = "10";
+let n = 10;
+document.writeln("s: ", s, typeof s);
+document.writeln("n: ", n, typeof n);
+document.writeln("s + n: ", s + n, typeof (s + n));
