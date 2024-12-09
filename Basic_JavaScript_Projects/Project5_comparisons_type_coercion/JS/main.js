68fb860fb33fc1530ab899ad256a3658e5ee2636
mouldimus <thomas@sportstore.co.uk>
2024-12-09 16:48:35 +0000

    Project5_Comparisons_Type_Coercion Assignment

--- Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,13 +1,17 @@
 let s = "10";
 let n = 10;
+// data types
 document.writeln("s: ", s, typeof s);
 document.writeln("n: ", n, typeof n);
+// type coercion
 document.writeln("s + n: ", s + n, typeof (s + n));
 
+// infinity
 let inf = 2e310;
 let negInf = -2e310;
 document.writeln("infinity: ", inf, typeof inf);
 document.writeln("negative infinity: ", negInf, typeof negInf);
+// comparisons and booleans
 document.writeln("5 < 3: ", 5 < 3);
 console.log("5 < 3: ", 5 < 3);
 console.log("s == n: ", s == n);
