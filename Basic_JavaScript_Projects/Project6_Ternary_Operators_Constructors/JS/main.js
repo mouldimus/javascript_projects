16124a6b338ee461e7c963be21030b4df50d8dc4
mouldimus <thomas@sportstore.co.uk>
2024-12-09 17:03:33 +0000

    Project6_Ternary_Operators_Constructors Assignment

---
+++ Basic_JavaScript_Projects/Project6_Ternary_Operators_Constructors/JS/main.js
@@ -0,0 +1,6 @@
+function rideFunction() {
+  let height, canRide;
+  height = document.getElementById("height").value;
+  canRide = height < 52 ? "You are too short" : "You are tall enough";
+  document.getElementById("ride").innerHTML = canRide + " to ride.";
+}
