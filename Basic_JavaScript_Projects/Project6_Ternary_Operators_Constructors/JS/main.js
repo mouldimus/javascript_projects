62d1ea1cce0ee69c9ada29564b5084f01130ad94
mouldimus <thomas@sportstore.co.uk>
2024-12-09 17:19:29 +0000

    Coding Assignment

--- Basic_JavaScript_Projects/Project6_Ternary_Operators_Constructors/JS/main.js
+++ Basic_JavaScript_Projects/Project6_Ternary_Operators_Constructors/JS/main.js
@@ -4,3 +4,22 @@ function rideFunction() {
   canRide = height < 52 ? "You are too short" : "You are tall enough";
   document.getElementById("ride").innerHTML = canRide + " to ride.";
 }
+
+function Vehicle(make, model, year, color) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.color = color;
+}
+let jack = new Vehicle("Dodge", "Viper", 2020, "red");
+let emily = new Vehicle("Jeep", "Trail Hawk", 2019, "blue");
+let erik = new Vehicle("Ford", "Pinto", 1971, "black");
+function myFunction() {
+  document.getElementById("keywordsAndConstructors").innerHTML =
+    "Erik drives a " +
+    erik.color +
+    "-coloured " +
+    erik.model +
+    " manufactured in " +
+    erik.year;
+}
