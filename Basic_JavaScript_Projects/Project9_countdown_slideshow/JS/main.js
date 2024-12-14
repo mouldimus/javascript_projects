ae21aba0fb014d5333ed9cebeff904a87a958305
mouldimus <thomas@sportstore.co.uk>
2024-12-14 11:47:16 +0000

    Project9_Countdown_Slideshow Assignment

--- Basic_JavaScript_Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic_JavaScript_Projects/Project9_countdown_slideshow/JS/main.js
@@ -1,15 +1,51 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
 function countdown() {
   let seconds = document.getElementById("seconds").value;
-
-  function tick() {
-    seconds--;
-    timer.innerHTML = seconds;
-    let time = setTimeout(tick, 1000);
-    if (seconds == -1) {
-      alert("Time's up!");
-      clearTimeout(time);
-      timer.innerHTML = "";
-    }
-    tick();
+  seconds = Number(seconds);
+  if (isNaN(seconds)) {
+    alert("Please input number of seconds");
+    document.getElementById("seconds").value = "";
+    return;
   }
+  let intervalId = setInterval(() => plusSlides(1), seconds * 1000);
+  let slides = document.getElementsByClassName("myslides");
+  setTimeout(
+    () => {
+      clearInterval(intervalId);
+    },
+    seconds * slides.length * 1000,
+  );
+}
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+  console.log("slideIndex: " + slideIndex);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("myslides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
