4f5e064825bdac570d447ccf1df55f572c5de473
mouldimus <thomas@sportstore.co.uk>
2024-12-14 09:54:23 +0000

    toPrecision() Method Assignment

--- Basic_JavaScript_Projects/Project8_string_methods/JS/main.js
+++ Basic_JavaScript_Projects/Project8_string_methods/JS/main.js
@@ -2,15 +2,20 @@ function concat() {
   var x = "Hello ";
   var y = "World!";
   var z = x.concat(y);
-  document.getElementById("demo").innerHTML = z;
+  document.getElementById("concat").innerHTML = z;
 }
 function slice() {
   let longString = "The quick brown fox jumps over the lazy dog.";
   let shortString = longString.slice(0, 10);
-  document.getElementById("sliceOutput").innerHTML = shortString;
+  document.getElementById("slice").innerHTML = shortString;
 }
 function stringMethod() {
   let x = 182;
   let y = x.toString();
   document.getElementById("numbersToString").innerHTML = y;
+}
+function precisionMethod() {
+  let x = 1234.56789;
+  let y = x.toPrecision(5);
+  document.getElementById("precision").innerHTML = y;
 }
