548a349c19afd9083f74ff9d96401d22ece65ab0
mouldimus <thomas@sportstore.co.uk>
2024-12-14 20:18:18 +0000

    For Loop Assignment

--- Basic_JavaScript_Projects/Project10_loops_arrays/JS/main.js
+++ Basic_JavaScript_Projects/Project10_loops_arrays/JS/main.js
@@ -1,7 +1,15 @@
 function callLoop() {
   let i = 0;
   while (i < 10) {
-    document.write(i, "<br />");
+    document.getElementById("loop").innerHTML += i + "<br />";
     i++;
+  }
+}
+
+function forLoop() {
+  let listOfInstruments = ["guitar", "bass", "drums", "keyboards"];
+  for (let i = 0; i < listOfInstruments.length; i++) {
+    document.getElementById("listOfInstruments").innerHTML +=
+      listOfInstruments[i] + "<br />";
   }
 }
