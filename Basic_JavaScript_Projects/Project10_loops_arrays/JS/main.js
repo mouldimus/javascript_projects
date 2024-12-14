c3349dd922e8e3fd8e888aaa25749f00c58dc911
mouldimus <thomas@sportstore.co.uk>
2024-12-14 20:33:28 +0000

    Const Keyword Assignment - Part 1

--- Basic_JavaScript_Projects/Project10_loops_arrays/JS/main.js
+++ Basic_JavaScript_Projects/Project10_loops_arrays/JS/main.js
@@ -18,3 +18,13 @@ function arrayFunction() {
   let arr = [10, 20, 30, 40, 50];
   document.getElementById("array").innerHTML = arr;
 }
+
+function constFunction() {
+  const car = {
+    type: "Fiat",
+    model: "500",
+    color: "white",
+  };
+  document.getElementById("constant").innerHTML =
+    "The car is a " + car.color + " " + car.type + " " + car.model;
+}
