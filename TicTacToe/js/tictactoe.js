be08ca4539df6b97df5ace129032a03746a39cd5
mouldimus <thomas@sportstore.co.uk>
2024-12-17 13:12:44 +0000

    Tic Tac Toe Assignment Part 5

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -65,3 +65,65 @@ function placeXOrO(squareNumber) {
     }
   }
 }
+
+// This function parses the selectedSquares array to search for win conditions.
+// drawLine() function is called to draw a line on the screen if the condition is met.
+function checkWinConditions() {
+  if (arrayIncludes("OX", "1X", "2X")) {
+    drawWinLine(50, 100, 558, 100);
+  } else if (arrayIncludes("3X", "4X", "5X")) {
+    drawWinLine(50, 304, 558, 304);
+  } else if (arrayIncludes("6X", "7X", "8X")) {
+    drawWinLine(50, 508, 558, 508);
+  } else if (arrayIncludes("0X", "3X", "6X")) {
+    drawWinLine(100, 50, 100, 558);
+  } else if (arrayIncludes("1X", "4X", "7X")) {
+    drawWinLine(304, 50, 508, 558);
+  } else if (arrayIncludes("2X", "5X", "8X")) {
+    drawWinLine(508, 50, 508, 558);
+  } else if (arrayIncludes("6X", "4X", "2X")) {
+    drawWinLine(100, 508, 510, 90);
+  } else if (arrayIncludes("0X", "4X", "8X")) {
+    drawWinLine(100, 100, 520, 520);
+  } else if (arrayIncludes("00", "10", "20")) {
+    drawWinLine(50, 100, 558, 304);
+  } else if (arrayIncludes("30", "40", "50")) {
+    drawWinLine(50, 304, 558, 304);
+  } else if (arrayIncludes("60", "70", "80")) {
+    drawWinLine(50, 508, 558, 508);
+  } else if (arrayIncludes("00", "30", "60")) {
+    drawWinLine(100, 50, 100, 558);
+  } else if (arrayIncludes("10", "40", "70")) {
+    drawWinLine(304, 50, 304, 558);
+  } else if (arrayIncludes("20", "50", "80")) {
+    drawWinLine(508, 50, 508, 558);
+  } else if (arrayIncludes("60", "40", "20")) {
+    drawWinLine(100, 508, 510, 90);
+  } else if (arrayIncludes("00", "40", "80")) {
+    drawWinLine(100, 100, 520, 520);
+  }
+  // This condition checks for a tie. If none of the above conditions are met and
+  // 9 squares are selected the code executes.
+  else if (selectedSquares.length >= 9) {
+    // This function plays the tie game sound.
+    audio("./media/tie.mp3");
+    // This function sets a .5 second timer before the resetGame is called.
+    setTimeout(function () {
+      resetGame();
+    }, 500);
+  }
+
+  // This function checks if an array includes 3 strings. It is used to check for
+  // each win condition.
+  function arrayIncludes(squareA, squareB, squareC) {
+    // These 3 variables will be used to check for 3 in a row.
+    const a = selectedSquares.includes(squareA);
+    const b = selectedSquares.includes(squareB);
+    const c = selectedSquares.includes(squareC);
+    // If the 3 variables we pass are all included in our array then
+    // true is returned and our else if condition executes the drawLine() function.
+    if (a === true && b === true && c === true) {
+      return true;
+    }
+  }
+}
