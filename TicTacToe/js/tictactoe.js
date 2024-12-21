12fe95adc24e6f7b8d1a5f47d0b33695e4a1b64e
mouldimus <thomas@sportstore.co.uk>
2024-12-21 15:29:02 +0000

    Tic Tac Toe Assignment

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -1,5 +1,7 @@
+const user = "O";
+const computer = user === "X" ? "O" : "X";
 // Player state variable
-let activePlayer = "X";
+let activePlayer = user;
 // This array stores an array of moves. We use this to determine win condition.
 let selectedSquares = [];
 
@@ -35,7 +37,7 @@ function placeXOrO(squareNumber) {
     // This function plays placement sound.
     audio("media/place.mp3");
     // This condition checks to see if it is the computer's turn.
-    if (activePlayer === "O") {
+    if (activePlayer === computer) {
       // This function disables clicking for the computers turn.
       disableClick();
       // This function waits 1 second before the computer places an image and enables click.
@@ -69,7 +71,7 @@ function placeXOrO(squareNumber) {
 // This function parses the selectedSquares array to search for win conditions.
 // drawLine() function is called to draw a line on the screen if the condition is met.
 function checkWinConditions() {
-  if (arrayIncludes("OX", "1X", "2X")) {
+  if (arrayIncludes("0X", "1X", "2X")) {
     drawWinLine(50, 100, 558, 100);
   } else if (arrayIncludes("3X", "4X", "5X")) {
     drawWinLine(50, 304, 558, 304);
@@ -78,28 +80,28 @@ function checkWinConditions() {
   } else if (arrayIncludes("0X", "3X", "6X")) {
     drawWinLine(100, 50, 100, 558);
   } else if (arrayIncludes("1X", "4X", "7X")) {
-    drawWinLine(304, 50, 508, 558);
+    drawWinLine(304, 50, 304, 558);
   } else if (arrayIncludes("2X", "5X", "8X")) {
     drawWinLine(508, 50, 508, 558);
   } else if (arrayIncludes("6X", "4X", "2X")) {
     drawWinLine(100, 508, 510, 90);
   } else if (arrayIncludes("0X", "4X", "8X")) {
     drawWinLine(100, 100, 520, 520);
-  } else if (arrayIncludes("00", "10", "20")) {
-    drawWinLine(50, 100, 558, 304);
-  } else if (arrayIncludes("30", "40", "50")) {
+  } else if (arrayIncludes("0O", "1O", "2O")) {
+    drawWinLine(50, 100, 558, 100);
+  } else if (arrayIncludes("3O", "4O", "5O")) {
     drawWinLine(50, 304, 558, 304);
-  } else if (arrayIncludes("60", "70", "80")) {
+  } else if (arrayIncludes("6O", "7O", "8O")) {
     drawWinLine(50, 508, 558, 508);
-  } else if (arrayIncludes("00", "30", "60")) {
+  } else if (arrayIncludes("0O", "3O", "6O")) {
     drawWinLine(100, 50, 100, 558);
-  } else if (arrayIncludes("10", "40", "70")) {
+  } else if (arrayIncludes("1O", "4O", "7O")) {
     drawWinLine(304, 50, 304, 558);
-  } else if (arrayIncludes("20", "50", "80")) {
+  } else if (arrayIncludes("2O", "5O", "8O")) {
     drawWinLine(508, 50, 508, 558);
-  } else if (arrayIncludes("60", "40", "20")) {
+  } else if (arrayIncludes("6O", "4O", "2O")) {
     drawWinLine(100, 508, 510, 90);
-  } else if (arrayIncludes("00", "40", "80")) {
+  } else if (arrayIncludes("0O", "4O", "8O")) {
     drawWinLine(100, 100, 520, 520);
   }
   // This condition checks for a tie. If none of the above conditions are met and
